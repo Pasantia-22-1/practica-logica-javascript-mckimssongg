084b6fc47bb85d4a2e172813fa69e6dd335f583a
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-03-18 17:07:00 -0600

    add

--- index.js
+++ index.js
@@ -11,33 +11,98 @@ function sizeOfEquation(equation){
 }
 
 function equationArr(equation){
-    ArrayAquation = equation.split('')
+    let ArrayAquation = equation.split('')
     return ArrayAquation
 }
 
-function calculated(size, Arrequation){
-    while (size != 1){
-        let currentSize = size
-        let chill = 0
-        for (let i = 0;  i < currentSize; i++){
-            if ((Arrequation[i] == "/" ) || (Arrequation[i] == "*")){
-                let n1 = Arrequation[i-1], n2 = Arrequation[i+1]
-                Arrequation[i] == "*" ? chill = Operacion.multiplicacion(n1, n2) : chill = Operacion.division(n1, n2)
-                equationUpdate = Arrequation.splice(i-1, 3,chill.toString());;
-                return equationUpdate;
-              
-            }else if((Arrequation[i] == "+" ) || (Arrequation[i] == "-")){
-                let n1 = Arrequation[i-1], n2 = Arrequation[i+1]
-                Arrequation[i] == "*" ? chill = Operacion.multiplicacion(n1, n2) : chill = Operacion.division(n1, n2)
-                equationUpdate = Arrequation.splice(i-1, 3,chill.toString());;
-                return Arrequation=equationUpdate;
-            }
+// --------------------------------------------------------------------------------------------------------------------------------
+function SearchSymbol(ArrayAquation){
+    for (let i = 0;  i < ArrayAquation.length; i++){
+        if ((ArrayAquation[i] == "/" ) || (ArrayAquation[i] == "*")){
+            return i;
+        }else if((ArrayAquation[i] == "+" ) || (ArrayAquation[i] == "-")){
+            return i;
+        }
     }
-    }
-    return Arrequation
+    return true;
+}
+    
+
+function Calculate(index, ArrayEquation){
+        let n1 = ArrayEquation[index-1], n2 = ArrayEquation[index+1]
+        let resultado
+        if (ArrayEquation[index] == "*" ){
+            resultado = Operacion.multiplicacion(n1, n2)
+         }else if(ArrayEquation[index] == "/" ){
+             resultado = Operacion.division(n1, n2)
+         }else if(ArrayEquation[index] == "+"){
+            resultado = Operacion.multiplicacion(n1, n2) 
+         }else if(ArrayEquation[index] == "-"){
+            resultado = Operacion.resta(n1, n2)
+         }
+         return resultado
 }
 
-let ecuacion1 = "4-7+8+9/2*3"
 
-let _size = sizeOfEquation(ecuacion1)
-console.log(_size)
+function replaceArray(resultado, ArrayEquation, index){
+    let equationUpdate = ArrayEquation.splice(index-1, 3,resultado.toString());;
+    return equationUpdate
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+let ecuacion1 = "4-7+8+9/2*3"
+let siNo = true
+if(ecuacion1.length > 20){
+    console.log("Faill")
+}
+else{
+    while(siNo){
+        let _equation = equationArr(ecuacion1)
+        console.log(_equation)
+        let _sizeEquation = sizeOfEquation(_equation)
+        console.log(_sizeEquation)
+        if(_sizeEquation == 1){
+        siNo = false 
+        console.log(_equation)
+        }else{
+            let index = SearchSymbol(_equation)
+            if (index == true){siNo = false}
+            else{
+                let res = Calculate(index, _equation)
+                _equation = replaceArray(res, _equation, index)
+            }
+        }
+    }
+}
+
+// // --------------------------------------------------------------------------------------------------------------------------------
+// function calculated(size, Arrequation){
+//     while (size != 1){
+//         let currentSize = size
+//         let chill = 0
+//         for (let i = 0;  i < currentSize; i++){
+//             if ((Arrequation[i] == "/" ) || (Arrequation[i] == "*")){
+//                 let n1 = Arrequation[i-1], n2 = Arrequation[i+1]
+//                 Arrequation[i] == "*" ? chill = Operacion.multiplicacion(n1, n2) : chill = Operacion.division(n1, n2)
+//                 equationUpdate = Arrequation.splice(i-1, 3,chill.toString());;
+//                 return Arrequation=equationUpdate;
+              
+//             }else if((Arrequation[i] == "+" ) || (Arrequation[i] == "-")){
+//                 let n1 = Arrequation[i-1], n2 = Arrequation[i+1]
+//                 Arrequation[i] == "+" ? chill = Operacion.suma(n1, n2) : chill = Operacion.resta(n1, n2)
+//                 equationUpdate = Arrequation.splice(i-1, 3,chill.toString());;
+//                 return Arrequation=equationUpdate;
+//             }
+//     }
+//     }
+//     return Arrequation
+// }
+// let ecuacion1 = "4-7+8+9/2*3"
+
+// let _size = sizeOfEquation(ecuacion1)
+// console.log(_size)
+// let _ArrayAquation = equationArr(ecuacion1)
+// console.log(_ArrayAquation)
+// let _resultado = calculated(_size, _ArrayAquation)
+// console.log(_resultado)
